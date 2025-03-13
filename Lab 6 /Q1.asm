9e40b4a5f9f77ab7b670d8d8df5dbbb8b3663144
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-03-13 12:36:27 +0500

    Update Q1.asm

--- Lab 6 /Q1.asm
+++ Lab 6 /Q1.asm
@@ -1,1 +1,21 @@
-asdad
+INCLUDE Irvine32.inc
+
+.code
+main PROC
+    mov ecx,10
+	mov ebx,0
+	mov eax,0
+	call WriteInt
+	mov eax, 1
+	call WriteInt
+
+	L1:
+	mov edx,eax
+	add eax,ebx
+	mov ebx, edx
+	call WriteInt	
+	
+	loop L1
+	exit
+main ENDP
+END main

5c1e2307bb8547974b1288974faa8d4bb9406b2e
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-03-18 20:22:47 +0500

    Create Q2(B).asm

---
+++ Lab 6 /Q2(B).asm
@@ -0,0 +1,20 @@
+INCLUDE Irvine32.inc
+
+.code
+main PROC
+    mov ecx,4
+	mov eax,1
+	mov ebx,4
+	L1:
+	mov edx,ecx
+	mov ecx,ebx
+	L2:
+	call writedec
+	loop L2
+	call crlf
+	mov ecx,edx
+	dec ebx
+	loop L1
+	exit
+main ENDP
+END main

69dddcf02b99e3fdaf642c926cac67cefcedc49c
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-04-24 14:22:53 +0500

    Update Q1.asm

--- Lab 8 /Q1.asm
+++ Lab 8 /Q1.asm
@@ -1,8 +1,10 @@
 INCLUDE Irvine32.inc
 .data
-	val1 BYTE ?
-	val2 BYTE ?
-	msg BYTE "Enter two numbers",0
+	val1 DWORD ?
+	val2 DWORD ?
+	val3 DWORD ?
+	val4 DWORD ?
+	msg BYTE "Enter Four Numbers",0
 	eq_msg BYTE "Equal numbers",0
 	neq_msg BYTE "Not Equal numbers",0
 .code
@@ -11,14 +13,45 @@ main PROC
 	call WriteString
 	call Crlf
 
-	call WriteInt
+	call ReadInt
 	mov val1,eax
-	call WriteInt
+	call ReadInt
 	mov val2,eax
+	call ReadInt
+	mov val3,eax
+	call ReadInt
+	mov val4,eax
+	mov eax,val1
+	mov ebx,val2
+	CMP eax,ebx	; val1 and val2
+	JNE eqq
+	mov ebx,val3
+	CMP eax,ebx	; val1 and val3
+	JNE eqq
+	mov ebx,val4
+	CMP eax,ebx	; val1 and val4
+	JNE eqq
+	mov eax,val2
+	mov ebx,val3
+	CMP eax,ebx	; val2 and val3
+	JNE eqq
+	mov ebx,val4
+	CMP eax,ebx	; val2 and val4
+	JNE eqq
+	mov eax,val3
+	mov ebx,val4
+	CMP eax,ebx	;val3 and val4
+	JNE eqq
+	mov edx, OFFSET eq_msg
+	call WriteString
+	call Crlf
+	exit
 
-	CMP val1,val2
-	JE eqq
-
+eqq:
+	mov edx, OFFSET neq_msg
+	call WriteString
+	call Crlf
+exit
 
 main ENDP
 END main

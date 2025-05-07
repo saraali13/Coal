53d45dd3dad37b3a3458777de94a72e5dbb15307
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-05-07 19:15:55 +0500

    Update Q1.asm

--- Lab 9 /Q1.asm
+++ Lab 9 /Q1.asm
@@ -1,1 +1,32 @@
+INCLUDE Irvine32.inc
 
+.data
+
+.code
+main PROC
+    push 5
+    push 7
+    push 3
+    
+    call ThreeProd
+    
+    INVOKE ExitProcess, 0
+main ENDP
+
+ThreeProd PROC
+    push ebp
+    mov ebp, esp
+    
+    ; Get parameters from stack
+    mov eax, [ebp+16]  
+    imul eax, [ebp+12] 
+    imul eax, [ebp+8]  
+    
+    ; Display result
+    call WriteInt
+    call Crlf
+    
+    pop ebp
+    ret 12              ; Clean up stack (3 params * 4 bytes each)
+ThreeProd ENDP
+END main

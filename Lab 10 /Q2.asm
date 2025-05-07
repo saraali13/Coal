8e2502ea6a9d4fa34828c81c0cf86244b2b60469
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-05-08 01:03:26 +0500

    Update Q2.asm

--- Lab 10 /Q2.asm
+++ Lab 10 /Q2.asm
@@ -7,8 +7,8 @@ notFoundMsg BYTE "Character not found",0
 
 .code
 main PROC
-    push OFFSET Str1       
-    push '#'               
+    push OFFSET Str1
+    push '#'
     call Scan_String_Param
     exit
 main ENDP
@@ -16,40 +16,45 @@ main ENDP
 Scan_String_Param PROC
     push ebp
     mov ebp, esp
-    push esi               
-    push ecx
+    push edi
     
-    mov esi, [ebp+12]      
-    mov al, [ebp+8]        
-    mov ecx, 0             
-
-SearchLoop:
-    mov dl, [esi]          
-    cmp dl, 0              
-    je NotFound
-    cmp dl, al             
-    je Found
-    inc esi
-    inc ecx
-    jmp SearchLoop
-
-Found:
+    cld                     ; Clear direction flag
+    mov edi, [ebp+12]     
+    mov al, [ebp+8]       
+    mov ecx, 0            
+    
+    ; Calculate string length first
+    push edi                ; Save original position
+    mov ecx, -1             ; Maximum possible length
+    xor al, al              ; Search for null terminator
+    repne scasb             ; Find string length
+    not ecx                 ; Convert to positive length
+    dec ecx                 ; Exclude null terminator
+    
+    pop edi                 ; Restore string address
+    mov al, [ebp+8]         ; Restore search character
+    repne scasb             ; Search for character
+    jnz NotFound
+    
+    ; Calculate position
+    mov eax, edi
+    sub eax, [ebp+12]
+    dec eax
+    
     mov edx, OFFSET foundMsg
     call WriteString
-    mov eax, ecx
     call WriteDec
     call Crlf
     jmp Done
-
+    
 NotFound:
     mov edx, OFFSET notFoundMsg
     call WriteString
     call Crlf
-
+    
 Done:
-    pop ecx
-    pop esi
+    pop edi
     pop ebp
-    ret 8                  
+    ret 8
 Scan_String_Param ENDP
 END main

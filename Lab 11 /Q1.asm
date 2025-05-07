08cda9d7cbd215b87e9ee73ce87213cfcce7fff9
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-05-07 19:13:36 +0500

    Update Q1.asm

--- Lab 11 /Q1.asm
+++ Lab 11 /Q1.asm
@@ -1,1 +1,17 @@
+INCLUDE Irvine32.inc
 
+.data
+
+.code
+main PROC
+
+; EAX * 21 = EAX * (16 + 4 + 1) = (EAX*16) + (EAX*4) + (EAX*1)
+mov ebx, eax   
+shl eax, 4     
+shl ebx, 2     
+add eax, ebx   
+add eax, edx   
+call WriteDec    
+    exit
+main ENDP
+END main
